16c6748bb9f379a9e4ec9211441fb2bc7b1be8ae
luciamoreira <55381118+luciamoreira@users.noreply.github.com>
2021-01-05 09:42:42 +0000

    Update BISGD.py

--- recommenders_implicit/BISGD.py
+++ recommenders_implicit/BISGD.py
@@ -3,11 +3,7 @@ from collections import defaultdict
 import random
 from data import ImplicitData
 import numpy as np
-from .Model2 import Model2 #mudar para model2 e criar model 2
-#from pprint import pprint ??
-
-#NrNodes=5
-#kappa = poisson.rvs(1, size=1)
+from .Model2 import Model2 
 
 class BISGD(Model2):
     def __init__(self, data: ImplicitData, num_factors: int = 10, num_iterations: int = 10, NrNodes: int = 5, learn_rate: float = 0.01, u_regularization: float = 0.1, i_regularization: float = 0.1, random_seed: int = 1, use_numba: bool = False):
@@ -31,25 +27,21 @@ class BISGD(Model2):
         self.use_numba = use_numba
         self.nrNodes = NrNodes
         np.random.seed(random_seed)
-        self._InitModel()  #inicia todos os nós (modelos) para já 5 modelos
-        #print(self.user_factors)
+        self._InitModel()  
+        
 
     def _InitModel(self):
         self.user_factors = defaultdict(dict)
         self.item_factors = defaultdict(dict)
-        self.counter=self.counter + 1
-        #print('InitModel', self.counter)
-        #print('data.userset no init', self.data.userset)
-        for node in range(self.nrNodes): #vou usar dicionário dentro de dicionário
-            for u in self.data.userset:
-                #self.user_factors[node] = {}
+        self.counter=self.counter + 1        
+        
+        for node in range(self.nrNodes): 
+            for u in self.data.userset:                
                 self.user_factors[node][u] = np.random.normal(0.0, 0.01, self.num_factors)
-                #print(self.user_factors[node][u])
-            for i in self.data.itemset:
-                #self.item_factors = {}
+            for i in self.data.itemset:                
                 self.item_factors[node][i] = np.random.normal(0.0, 0.01, self.num_factors)
-        #print('user_factors no init', self.user_factors)
-    def BatchTrain(self):
+        
+    def BatchTrain(self): # not modified
         """
         Trains a new model with the available data.
         """
@@ -68,23 +60,18 @@ class BISGD(Model2):
         user_id -- The ID of the user
         item_id -- The ID of the item
         """
-        #self.node = node
-        self.counter=self.counter+1
-        #print('IncrTrain counter node user id', self.counter, node, user_id)
+        
+        self.counter=self.counter+1        
         kappa = int(poisson.rvs(1, size=1))
-        #print('kappa', kappa)
-
+        
         if kappa == 0:
 
             if user_id not in self.data.userset or entra_uid < self.nrNodes:
-                self.user_factors[node][user_id] = np.random.normal(0.0, 0.01, self.num_factors)
-                    #print(self.user_factors[node][user_id])
+                self.user_factors[node][user_id] = np.random.normal(0.0, 0.01, self.num_factors)                    
             if item_id not in self.data.itemset or entra_iid < self.nrNodes:
                 self.item_factors[node][item_id] = np.random.normal(0.0, 0.01, self.num_factors)
             self.data.AddFeedback(user_id, item_id)
-            #self._UpdateFactors(user_id, item_id, node)
-
-
+            
 
         if kappa > 0:
 
@@ -96,15 +83,14 @@ class BISGD(Model2):
                     self.item_factors[node][item_id] = np.random.normal(0.0, 0.01, self.num_factors)
                 self.data.AddFeedback(user_id, item_id)
                 self._UpdateFactors(user_id, item_id, node)
-        #print('user factors inctrain', self.user_factors)
+        
 
     def _UpdateFactors(self, user_id, item_id, node, update_users: bool = True, update_items: bool = True, target: int = 1):
         self.counter=self.counter+1
-        #print('updatefactors, userid, node',self.counter, user_id, node)
-        p_u = self.user_factors[node][user_id]
-        #print('pu_antesupdate', len(p_u))
+        
+        p_u = self.user_factors[node][user_id]        
         q_i = self.item_factors[node][item_id]
-        #print('qi_antesupdate', len(q_i))
+        
         for _ in range(int(self.num_iterations)):
             err = target - np.inner(p_u, q_i)
 
@@ -116,13 +102,10 @@ class BISGD(Model2):
                 delta = self.learn_rate * (err * p_u - self.item_regularization * q_i)
                 q_i += delta
 
-        self.user_factors[node][user_id] = p_u
-        #print('pu_depoisupdate', len(p_u))
-        #print('user_factors', self.user_factors)
+        self.user_factors[node][user_id] = p_u      
+        
         self.item_factors[node][item_id] = q_i
-        #print('qi_depoisupdate', len(q_i))
-
-
+        
     def Predict(self, user_id, item_id, node):
         """
         Return the prediction (float) of the user-item interaction score.
@@ -143,32 +126,23 @@ class BISGD(Model2):
         user_id -- The ID of the user
         item_id -- The ID of the item
         """
-        #print('node no Reccomend', node) #entrou bem
-        #print('user_id', user_id) #entrou bem
-        #print('self.data.userset', list(self.data.userset)) #dá vazio...
+        
         recs=dict()
         recs[node] = []
         lista=[]
-        #self.counter=self.counter+1
-        #print('recommend', self.counter)
-        #.
-        #for i in range(self.nrNodes):
 
         if(user_id in self.data.userset and entra_uid >= self.nrNodes):
-            #print('entrei') # passa aqui...
+            
             self.counter=self.counter+1
-            #print('recommend_if1', self.counter)
-            #print(self.user_factors[node][user_id])
+
             if len(candidates) == 0:
                 candidates = self.data.itemset
 
             if exclude_known_items:
                 candidates = candidates - set(self.data.GetUserItems(user_id))
 
-            #print('user factors no recomend', self.user_factors)
-            #print('list(user factors)', list(self.user_factors.values()))
-            p_u = self.user_factors[node][user_id] # dá erro porque a inicialização do node 1 o node 1 não foi criado
-            #print('recommend', p_u)
+            p_u = self.user_factors[node][user_id] 
+            
             itemlist = np.array(list(self.item_factors[node].keys()))
             factors = np.array(list(self.item_factors[node].values()))
             """if self.use_numba:
@@ -184,16 +158,11 @@ class BISGD(Model2):
 
         if n == -1 or n > len(recs[node]):
             self.counter=self.counter+1
-            #print('recommend_if2', self.counter)
-            #print('recs', recs)
-            n = len(recs[node])
+             n = len(recs[node])
 
 
         if entra_uid < self.nrNodes:
             self.counter=self.counter+1
-            #print('recommend_if3', self.counter)
-            #print('recs', recs)
             n = len(recs[node])
-
 
         return recs[node][:n]
