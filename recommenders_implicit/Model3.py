9ac189924a2dcf7fe51996d7eff800293823620f
luciamoreira <55381118+luciamoreira@users.noreply.github.com>
2021-01-05 09:14:44 +0000

    Original and modified files

---
+++ recommenders_implicit/Model3.py
@@ -0,0 +1,43 @@
+class Model3:
+
+    def __init__(self):
+        pass
+
+    def BatchTrain(self):
+        """
+        Trains a new model with the available data.
+        """
+        pass
+
+    def IncrTrain(self, user_id, item_id, node, kappa, update_users: bool = True, update_items: bool = True):
+        """
+        Incrementally updates the model.
+
+        Keyword arguments:
+        user_id -- The ID of the user
+        item_id -- The ID of the item
+        """
+        pass
+
+    def IncrTrain2(self, user_id, item_id, node, update_users: bool = True, update_items: bool = True):
+        pass
+
+    def Predict(self, user_id, item_id, node):
+        """
+        Return the prediction (float) of the user-item interaction score.
+
+        Keyword arguments:
+        user_id -- The ID of the user
+        item_id -- The ID of the item
+        """
+        pass
+
+    def Recommend(self, user_id: int, item_id: int, node: int, kappa: int, n: int = -1, candidates: set = {}, exclude_known_items: bool = True):
+        """
+        Returns an list of tuples in the form (item_id, score), ordered by score.
+
+        Keyword arguments:
+        user_id -- The ID of the user
+        item_id -- The ID of the item
+        """
+        pass
---
+++ recommenders_implicit/__init__.py
@@ -0,0 +1,3 @@
+from .Model import Model
+from .ISGD import ISGD
+from .RAISGD import RAISGD
